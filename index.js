2264a0583be107450d06dd0605aab5bbdada3fb5
JohanIbarra <johangabrielibarra6.4@gmail.com>
2023-06-06 17:59:10 -0500

    subir archivos

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ window.onscroll = function(){
         header.classList.remove("nav_mod");
     }
 }
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario
+  
+    var nombre = document.getElementById("nombre").value;
+    var archivo = document.getElementById("archivo").files[0];
+    
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = "Nombre: " + nombre + "<br>Archivo: " + archivo.name;
+    resultado.addEventListener("click", function() {
+      var url = URL.createObjectURL(archivo);
+      var a = document.createElement("a");
+      a.href = url;
+      a.download = archivo.name;
+      a.click();
+      URL.revokeObjectURL(url);
+    });
+  });
+  
 
 
 
